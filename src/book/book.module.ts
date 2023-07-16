ea09824b62072e2c5f90643d2778c95c85f29b62
Vikash Gupta <vikashg2512@gmail.com>
2023-07-16 14:58:57 +0530

    global and module middleware setup

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
+import { BookMiddleWare } from './book.middleware';
 
 @Module({
   imports: [],
   controllers: [BookController],
   providers: [BookService],
 })
-export class BookModule {}
+export class BookModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(BookMiddleWare).forRoutes('book');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NextFunction, Request, Response } from 'express';
+
+// function globalMiddleWare(req: Request, res: Response, next: NextFunction) {
+//   console.log('global middleware 1');
+//   next();
+// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.use(globalMiddleWare);
   await app.listen(3000);
 }
 bootstrap();
